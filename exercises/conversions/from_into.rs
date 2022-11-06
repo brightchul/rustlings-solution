ad436026e94bc57cb6bb8fa53bcca3d8f950968b
brightchul <abhbtbb1@gmail.com>
2022-11-06 19:28:38 +0900

    style: remove comment

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,8 +35,6 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
         // TODO: I want to know the correct way.
